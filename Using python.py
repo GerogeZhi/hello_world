60d8da5aacb0c9cc73377623160fd52d69c8e674
GeorgeZhi <1021929607@qq.com>
2018-03-20 12:04:41 +0800

    Update Using python.py

--- Using python.py
+++ Using python.py
@@ -411,7 +411,6 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 
 proj=ccrs.Mercator()
-
 plt.figure(figsize=(10,10))
 ax=plt.axes(projection=proj)
 ax.set_extent((-25,20,52,10))
@@ -426,5 +425,43 @@ for name in bird_names:
     x,y=birddata.longitude[ix],birddata.latitude[ix]
     ax.plot(x,y,transform=ccrs.Geodetic(),label=name)
             
+# random graphs
 
+import networkx as nx
+from scipy.stats import bernoulli
+bernoulli.rvs(p=0.2)
+def er_graph(N,p):
+    '''generate an ER graph'''
+    G=nx.Graph()
+    G.add_nodes_from(range(N))
+    for node1 in G.nodes():
+        for node2 in G.nodes():
+            if node1 < node2 and bernoulli.rvs(p=p):
+                G.add_edge(node1,node2)
+    return G
+nx.draw(er_graph(50,0.08),node_size=40,node_color='red')
+plt.savefig('er1.pdf')
+
+# Social Networkx Model
+
+import networkx as nx
+G=nx.Graph()
+G.add_node(1)
+G.add_nodes_from([2,3])
+G.add_nodes_from(['u','v'])
+G.add_edge(1,2)
+G.add_edge('u','v')
+G.add_edges_from([(1,3),(1,4),(1,5),(1,6)])
+G.remove_node(2)
+G.remove_nodes_from([4,5])
+G.remove_edge(1,3)
+G.remove_edges_from([(1,2),('u','v')])
+G.number_of_edges()
+
+#visualize a graph
+G=nx.karate_club_graph()
+nx.draw(G,with_labels=True,node_color='lightblue',edge_colors='red')
+plt.savefig('karate_graph.pdf')
+G.degree()[33]
+G.degree(33)
 
